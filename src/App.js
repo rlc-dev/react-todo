10bb5664c71c0142fc8add6266e59f82249a86f8
Ray from Digify <rl-digify@protonmail.com>
2021-05-13 11:49:44 +0200

    Added todo and todolist starting point which now needs some checks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import TodoList from "./TodoList"
 
 function App() {
-  return (
-    
-  );
+  const [todos, setTodos] = useState([])
+
+	return (
+		<>
+			<TodoList todos={todos} />
+			<input type="text" />
+      <button>Add new item</button>
+      <button>Remove done items</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do.</div>
+		</>
+	);
 }
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Todo() {
-	return <div></div>;
+export default function Todo({ todo }) {
+    
+	return (
+		<div>
+			<label>
+				<input type="checkbox" checked={todo.complete} />
+				{todo.name}
+			</label>
+		</div>
+	);
 }
