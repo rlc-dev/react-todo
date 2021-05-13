e66b281f61dbbf627ec4d9594be2e6b98d76d688
Ray from Digify <rl-digify@protonmail.com>
2021-05-13 13:36:45 +0200

    Initial toggle functionality added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import TodoList from "./TodoList"
 function App() {
   const [todos, setTodos] = useState([])
 
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+
 	return (
 		<>
-			<TodoList todos={todos} />
+			<TodoList todos={todos} toggleTodo={toggleTodo} />
 			<input type="text" />
-      <button>Add new item</button>
-      <button>Remove done items</button>
+      <button>Add Todo</button>
+      <button>Clear Complete</button>
       <div>{todos.filter(todo => !todo.complete).length} left to do.</div>
 		</>
 	);
