f4bab8cfc92f5ba59e3fa48078010ca7417285fa
Ray from Digify <rl-digify@protonmail.com>
2021-05-13 14:04:06 +0200

    Added unique ids, button toggles and localstorage required for saving the state of todos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList"
+import uuidv4 from 'uuid/dist/v4'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify (todos))
+  }, [todos])
 
   function toggleTodo(id) {
     const newTodos = [...todos]
@@ -11,13 +24,26 @@ function App() {
     setTodos(newTodos)
   }
 
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
+    })
+    todoNameRef.current.value = null
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
 
 	return (
 		<>
 			<TodoList todos={todos} toggleTodo={toggleTodo} />
-			<input type="text" />
-      <button>Add Todo</button>
-      <button>Clear Complete</button>
+			<input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add new item</button>
+      <button onClick={handleClearTodos}>Remove done items</button>
       <div>{todos.filter(todo => !todo.complete).length} left to do.</div>
 		</>
 	);
